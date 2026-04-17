2ad66252eda33c16872116fcd580daeacbade626
NarezIn <zn2133@nyu.edu>
2026-04-16 23:40:25 -0400

    Update transcribe audio logic in flask backend - send request and return transcript

---
+++ web-app/app/services.py
@@ -0,0 +1,53 @@
+"""
+Handles communication with the speech-to-text, text analysis, and LLM services.
+
+Currently uses a stub implementation.
+"""
+import requests
+
+# We will replace this with the actual ML service URL once it's implemented
+ML_URL = "http://url-placeholder"
+
+def transcribe_audio(file):
+    """
+    Sends raw audio file as a request to ML client, 
+    and retrieves the transcribed text.
+
+    Args:
+        file: Uploaded raw audio file
+        (file: FileStorage object from Flask (request.files["audio"]))
+
+    Returns:
+        str: Transcribed text
+    """
+    url = ML_URL # or f"{ML_BASE_URL}/transcribe"
+
+    files = {
+        "file": (file.filename, file.stream, file.mimetype)
+    }
+    # represents: (filename, (actual) file_object, content_type (like wav/mp3))
+
+    try:
+        response = requests.post(url, files=files, timeout=10)
+    except requests.exceptions.RequestException as e:
+        raise requests.exceptions.RequestException("Failed to connect to ML service:", e)
+
+    data = response.json()
+
+    return data.get("transcript", "")
+
+def analyze_text(transcript):
+    """
+    STUB code right now.
+    Simulates LLM-based speech analysis.
+
+    Args:
+        transcript (str): Raw transcription
+
+    Returns:
+        dict: Analysis results including cleaned text and feedback
+    """
+    return {
+        "cleaned_text": "This is a stub transcription.",
+        "feedback": "stub response."
+    }
